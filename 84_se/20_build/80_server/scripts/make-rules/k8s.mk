e591ce161c43b72998ba989ef7b08db83893553a
rebirthmonkey <rebirthmonkey@gmail.com>
2023-04-21 09:20:14 +0800

    update k8s deploy

--- 84_se/20_build/80_server/scripts/make-rules/k8s.mk
+++ 84_se/20_build/80_server/scripts/make-rules/k8s.mk
@@ -24,8 +24,11 @@ k8s.deploy: $(addprefix k8s.deploy., $(DEPLOYS))
 
 .PHONY: k8s.deploy.%
 k8s.deploy.%:
-	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
 	@echo "===========> K8s Deploying $* $(VERSION)-$(ARCH)"
+	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
+	$(shell ./scripts/conf2yaml.sh)
+	$(KUBECTL) -n $(NAMESPACE) apply -f manifests/config.yaml
+	$(KUBECTL) -n $(NAMESPACE) apply -f manifests/cert.yaml
 	# echo @$(KUBECTL) -n $(NAMESPACE) set image deployment/$* $*=$(REGISTRY_PREFIX)/$*-$(ARCH):$(VERSION)
 	$(KUBECTL) -n $(NAMESPACE) apply -f manifests/$*.yaml
 
@@ -37,3 +40,6 @@ k8s.undeploy.%:
 	$(eval ARCH := $(word 2,$(subst _, ,$(PLATFORM))))
 	@echo "===========> K8s Undeploying $* $(VERSION)-$(ARCH)"
 	@$(KUBECTL) -n $(NAMESPACE) delete -f manifests/$*.yaml
+	$(KUBECTL) -n $(NAMESPACE) delete -f manifests/cert.yaml
+	$(KUBECTL) -n $(NAMESPACE) delete -f manifests/config.yaml
+
